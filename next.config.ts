6140185d7c054d505eb3d5002c64589ea9d25cbe
Simone Taeggi <startae14@gmail.com>
2025-12-13 23:13:50 +0100

    00 starting setup project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import { dirname } from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: dirname(__filename),
+  }
 };
 
 export default nextConfig;
